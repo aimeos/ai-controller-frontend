70406f7a8cda959e4dad77b5105adc11702b29a8
Aimeos <aimeos@aimeos.org>
2016-12-07 22:59:28 +0100

    Streamlined file header and updated copyright year

--- controller/frontend/tests/Controller/Frontend/Service/Decorator/ExampleTest.php
+++ controller/frontend/tests/Controller/Frontend/Service/Decorator/ExampleTest.php
@@ -4,9 +4,9 @@ namespace Aimeos\Controller\Frontend\Plugin\Decorator;
 
 
 /**
- * @copyright Metaways Infosystems GmbH, 2012
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015
+ * @copyright Metaways Infosystems GmbH, 2012
+ * @copyright Aimeos (aimeos.org), 2015-2016
  */
 class ExampleTest extends \PHPUnit_Framework_TestCase
 {

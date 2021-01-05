13d657143bbdee5dfbaeea9fdb37de58acf78c91
Aimeos <aimeos@aimeos.org>
2021-01-05 19:26:48 +0100

    Copyright update

--- controller/frontend/tests/Controller/Frontend/Basket/FactoryTest.php
+++ controller/frontend/tests/Controller/Frontend/Basket/FactoryTest.php
@@ -6,7 +6,7 @@ namespace Aimeos\Controller\Frontend\Basket;
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Metaways Infosystems GmbH, 2012
- * @copyright Aimeos (aimeos.org), 2015-2020
+ * @copyright Aimeos (aimeos.org), 2015-2021
  */
 class FactoryTest extends \PHPUnit\Framework\TestCase
 {

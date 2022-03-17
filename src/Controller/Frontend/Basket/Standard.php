870725b8543ecf0f583dc37cda183b05963da521
Aimeos <aimeos@aimeos.org>
2022-03-17 19:15:41 +0100

    Pass config options to calcPrice() of service providers

--- src/Controller/Frontend/Basket/Standard.php
+++ src/Controller/Frontend/Basket/Standard.php
@@ -423,7 +423,7 @@ class Standard
 		}
 
 		// remove service rebate of original price
-		$price = $provider->calcPrice( $this->get() )->setRebate( '0.00' );
+		$price = $provider->calcPrice( $this->get(), $config )->setRebate( '0.00' );
 
 		$orderBaseServiceManager = \Aimeos\MShop::create( $context, 'order/base/service' );
 

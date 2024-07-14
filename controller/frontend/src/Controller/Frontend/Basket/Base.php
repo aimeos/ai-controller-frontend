c8ccfeb3f40ef2f5db721e4459ece3451d76ffa7
Aimeos <aimeos@aimeos.org>
2024-07-14 09:57:42 +0200

    Avoid error when migrating baskets due to services added by Autofill plugin

--- controller/frontend/src/Controller/Frontend/Basket/Base.php
+++ controller/frontend/src/Controller/Frontend/Basket/Base.php
@@ -308,10 +308,12 @@ abstract class Base extends \Aimeos\Controller\Frontend\Base implements Iface
 			{
 				try
 				{
+					$position = null;
+
 					foreach( $newBasket->get()->getService( $type ) as $pos => $ordService )
 					{
 						if( $item->getCode() === $ordService->getCode() ) {
-							$newBasket->get()->deleteService( $type, $pos );
+							$position = $pos;
 						}
 					}
 
@@ -322,7 +324,7 @@ abstract class Base extends \Aimeos\Controller\Frontend\Base implements Iface
 					}
 
 					$service = $manager->get( $item->getServiceId(), ['media', 'price', 'text'] );
-					$newBasket->addService( $service, $attributes );
+					$newBasket->addService( $service, $attributes, $position );
 					$basket->deleteService( $type );
 				}
 				catch( \Exception $e ) { ; } // Don't notify the user as appropriate services can be added automatically
